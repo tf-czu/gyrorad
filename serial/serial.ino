72d3f0ef2ab02943465f04b16430623d67d49a3d
martind <martind@mapfactor.com>
2015-03-31 15:39:58 +0200

    sending time,acc,temp,gyro CSV + GPS at 38400

--- serial/serial.ino
+++ serial/serial.ino
@@ -31,7 +31,7 @@ GPSSerial gps;
 //const int MPU=0x68;  // I2C address of the MPU-6050
 const int MPU=0x69;  // I2C address of the MPU-6050 for AD0=1
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-
+long time;
 
 void setup()
 {
@@ -56,17 +56,18 @@ void setup()
   }
   
   
-  Serial.begin( 9600 );
-  Serial.print( "I2C test ...\n" );
+  Serial.begin( 38400 );
+  Serial.print( "\n\nid,timeMs,accX,accY,accZ,temp,gyroX,gyroY,gyroZ\n" );
 }
 
 void readIthGyro( int i )
 {
   digitalWrite( gyroSelectPins[i], 1 );  // select i-th gyro
-  delay(10);
+  delay(1);
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
+  time = millis();
   Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
@@ -76,7 +77,7 @@ void readIthGyro( int i )
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
   digitalWrite( gyroSelectPins[i], 0 ); // unselect
-  delay(10);
+  delay(1);
 }
 
 void loop()
@@ -85,16 +86,17 @@ void loop()
   for( i = 0; i < NUM_GYROS; i++ )
   {
     readIthGyro( i );
-    Serial.print( i ); Serial.print(": ");
-    Serial.print("AcX = "); Serial.print(AcX);
-    Serial.print(" | AcY = "); Serial.print(AcY);
-    Serial.print(" | AcZ = "); Serial.println(AcZ);
-//  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-//  Serial.print(" | GyX = "); Serial.print(GyX);
-//  Serial.print(" | GyY = "); Serial.print(GyY);
-//  Serial.print(" | GyZ = "); Serial.println(GyZ);
+    Serial.print( i );
+    Serial.print(","); Serial.print(time);
+    Serial.print(","); Serial.print(AcX);
+    Serial.print(","); Serial.print(AcY);
+    Serial.print(","); Serial.print(AcZ);
+    Serial.print(","); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+    Serial.print(","); Serial.print(GyX);
+    Serial.print(","); Serial.print(GyY);
+    Serial.print(","); Serial.println(GyZ);
   }
-  gps.process();
-  delay(333);
+  //gps.process();
+  //delay(50);
 }
 
