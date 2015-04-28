eb1c30a9715857a50631de170cecb443457c2752
martind <martind@mapfactor.com>
2015-04-28 15:05:09 +0200

    added parameter "all" and prepared code for interleave of GPS data with the Gyro/Acc

--- serial/splitter.py
+++ serial/splitter.py
@@ -2,7 +2,7 @@
 """
   Split logged data into separate "channels"
   usage:
-       ./splitter.py <log file> <GPS|0..3>
+       ./splitter.py <log file> <GPS|0..3|all>
 """
 import sys
 
@@ -10,54 +10,6 @@ FIRST_LINE = "id,timeMs,accX,accY,accZ,temp,gyroX,gyroY,gyroZ\n"
 
 GPS_SEPARATOR_BEGIN = chr(0x2)
 GPS_SEPARATOR_END   = chr(0x3)
-
-def stripHeader( data ):
-    if FIRST_LINE in data:
-        return data[ data.find(FIRST_LINE) + len(FIRST_LINE): ]
-    return data
-
-def splitter( data, selected ):
-    assert selected in ['GPS','0','1','2','3'], selected
-    selectedGPS = selected == 'GPS'
-    gpsSection = False
-    data = stripHeader( data )
-    result = ""
-    for line in data.split('\n'):
-        if GPS_SEPARATOR_BEGIN in line:
-            if GPS_SEPARATOR_END in line:
-                if selectedGPS:
-                    result += line.split(GPS_SEPARATOR_BEGIN)[1].split(GPS_SEPARATOR_END)[0]
-                else:
-                    line = line.split(GPS_SEPARATOR_BEGIN)[0] + line.split(GPS_SEPARATOR_END)[1]
-                gpsSection = False
-            else:
-                if selectedGPS:
-                    result += line.split(GPS_SEPARATOR_BEGIN)[1]
-                else:
-                    line = line.split(GPS_SEPARATOR_BEGIN)[0]
-                gpsSection = True
-        elif GPS_SEPARATOR_END in line:
-            if selectedGPS:
-                result += line.split(GPS_SEPARATOR_END)[0]
-            else:
-                line = line.split(GPS_SEPARATOR_END)[1]
-            gpsSection = False
-        elif gpsSection:
-            if selectedGPS:
-                result += line.strip() + '\n'
-            else:
-                line = ""
-            
-        if len(line.split(',')) >= 9:
-            if line[:2] not in ['0,','1,','2,','3,']:
-                parts = line.split(',')
-                s = parts[-9]
-                if len(s) > 0:
-                    line = parts[-9][-1] + ',' + ",".join( parts[-8:] )
-            if line.startswith( selected ) and '*' not in line:
-                result += line.strip() + '\n'
-    return result
-
 
 
 def checksum( s ):
@@ -89,16 +41,74 @@ def parseNMEA( data ):
     return ret
 
 
+def stripHeader( data ):
+    if FIRST_LINE in data:
+        return data[ data.find(FIRST_LINE) + len(FIRST_LINE): ]
+    return data
+
+def splitter( data, selected ):
+    assert selected in ['GPS','0','1','2','3','ALL'], selected
+    gpsSection = False
+    data = stripHeader( data )
+    result, resultGPS = "", ""
+    lastGPS = None
+    records = []
+    lastSeek = 0
+    for line in data.split('\n'):
+        if GPS_SEPARATOR_BEGIN in line:
+            if GPS_SEPARATOR_END in line:
+                resultGPS += line.split(GPS_SEPARATOR_BEGIN)[1].split(GPS_SEPARATOR_END)[0]
+                line = line.split(GPS_SEPARATOR_BEGIN)[0] + line.split(GPS_SEPARATOR_END)[1]
+                gpsSection = False
+            else:
+                resultGPS += line.split(GPS_SEPARATOR_BEGIN)[1]
+                line = line.split(GPS_SEPARATOR_BEGIN)[0]
+                gpsSection = True
+        elif GPS_SEPARATOR_END in line:
+            resultGPS += line.split(GPS_SEPARATOR_END)[0]
+            line = line.split(GPS_SEPARATOR_END)[1]
+            gpsSection = False
+        elif gpsSection:
+            resultGPS += line.strip() + '\n'
+            line = ""
+            
+        arr = parseNMEA( resultGPS[lastSeek:] )
+        if len(arr) > 0 and arr[-1] != lastGPS:
+            lastSeek = max(0, len(resultGPS)-80) # max NMEA line is 80 characters
+            lastGPS = arr[-1]
+            records.append( lastGPS )
+
+        if len(line.split(',')) >= 9:
+            if line[:2] not in ['0,','1,','2,','3,']:
+                parts = line.split(',')
+                s = parts[-9]
+                if len(s) > 0:
+                    line = parts[-9][-1] + ',' + ",".join( parts[-8:] )
+            if line.startswith( selected ) and '*' not in line:
+                result += line.strip() + '\n'
+            records.append( [int(x) for x in line.split(',') if '.' not in x] ) # ignore float temperature
+    if selected == 'GPS':
+        return resultGPS
+    if selected == 'ALL':
+        return records
+    return result
+
+
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print __doc__
         sys.exit(2)
-    selected=sys.argv[2].upper()
+    selected = sys.argv[2].upper()
     data = splitter( open(sys.argv[1], "rb").read(), selected=selected )
     if selected == "GPS":
         print data
         print "------------------"
         print parseNMEA( data )
+    elif selected == "ALL":
+        for row in data:
+            print row
     else:
         print data
 
